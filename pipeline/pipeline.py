4e66d0413f19777db2f5527aa49dab2e8c1c7681
Moses Mhango <46014443+Moses21@users.noreply.github.com>
2026-01-17 14:29:24 +0200

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,20 +1,3 @@
-# import sys
-
-# print("arguments", sys.argv)
-
-# if len(sys.argv) < 2:
-#     print("Usage: python pipeline.py <month>")
-#     sys.exit(1)
-
-# month = int(sys.argv[1])
-# print(f"hello pipeline, month={month}")
-# import pandas as pd
-
-# df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
-# print(df.head())
-
-# df.to_parquet(f"output_{month}.parquet")
-
 import sys
 import pandas as pd
 
